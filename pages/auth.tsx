a2d25d7163f5075c6059bc27245e0246033cd135
IgorTheFixer <75816411+IgorTheFixer@users.noreply.github.com>
2023-06-07 15:42:24 -0400

    fixed signin bug

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,8 +2,11 @@ import { useCallback, useState } from "react";
 import Input from "@/components/Input"
 import axios from 'axios'
 import { signIn } from 'next-auth/react'
+import { useRouter } from "next/router";
 
 const Auth = () =>{
+  const router = useRouter()
+
   const [email, setEmail] = useState('')
   const [name, setName] = useState('')
   const [password, setPassword] = useState('')
@@ -13,18 +16,6 @@ const Auth = () =>{
   const toggleMode = useCallback(()=>{
     setMode((currentMode)=> currentMode === 'login' ? 'register' : 'login')
   },[])
-  
-  const register = useCallback(async () => {
-    try {
-      await axios.post('/api/register',{
-        email,
-        name,
-        password
-      })
-    } catch (error){
-      console.log(error);
-    }
-  },[email, name, password])
 
   const login = useCallback(async () =>{
     try {
@@ -34,10 +25,26 @@ const Auth = () =>{
         redirect: false,
         callbackUrl: '/'
       })
+
+      router.push('/')
     } catch(error) {
       console.log(error)
     }
-  },[email, password])
+  },[email, password, router])
+  
+  const register = useCallback(async () => {
+    try {
+      await axios.post('/api/register',{
+        email,
+        name,
+        password
+      })
+      debugger
+      login()
+    } catch (error){
+      console.log(error);
+    }
+  },[email, name, password, login])
 
   return(
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -74,7 +81,7 @@ const Auth = () =>{
                 value={password}
               />
             </div>
-            <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button onClick={mode === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
               {mode === 'login' ? 'Login' : 'Sign up'}
             </button>
             <p className="text-neutral-500 mt-12">
