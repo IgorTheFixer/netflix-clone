189a8031125c6f7d32f766a8004c9cc361ac8ff2
IgorTheFixer <75816411+IgorTheFixer@users.noreply.github.com>
2023-06-07 13:04:27 -0400

    implemented signin functionality

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useState } from "react";
 import Input from "@/components/Input"
 import axios from 'axios'
+import { signIn } from 'next-auth/react'
 
 const Auth = () =>{
   const [email, setEmail] = useState('')
@@ -14,8 +15,29 @@ const Auth = () =>{
   },[])
   
   const register = useCallback(async () => {
+    try {
+      await axios.post('/api/register',{
+        email,
+        name,
+        password
+      })
+    } catch (error){
+      console.log(error);
+    }
+  },[email, name, password])
 
-  },[])
+  const login = useCallback(async () =>{
+    try {
+      await signIn('credentials',{
+        email,
+        password,
+        redirect: false,
+        callbackUrl: '/'
+      })
+    } catch(error) {
+      console.log(error)
+    }
+  },[email, password])
 
   return(
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -52,7 +74,7 @@ const Auth = () =>{
                 value={password}
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
               {mode === 'login' ? 'Login' : 'Sign up'}
             </button>
             <p className="text-neutral-500 mt-12">
