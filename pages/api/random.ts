369c0ba1c951894bd67a6b5288d20f534a3440d2
IgorTheFixer <75816411+IgorTheFixer@users.noreply.github.com>
2023-06-09 13:57:57 -0400

    adding another hook to fetch a random movie

---
+++ pages/api/random.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import db from '@/lib/db';
+import serverAuth from "@/lib/serverAuth";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    if (req.method !== 'GET') {
+      return res.status(405).end();
+    }
+
+    await serverAuth(req, res);
+
+    const moviesCount = await db.movie.count();
+    const randomIndex = Math.floor(Math.random() * moviesCount);
+
+    const randomMovies = await db.movie.findMany({
+      take: 1,
+      skip: randomIndex
+    });
+
+    return res.status(200).json(randomMovies[0]);
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).end();
+  }
+}
