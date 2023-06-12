5fb1761e17d0624fbb8f1be10d95eb069c79dcc7
IgorTheFixer <75816411+IgorTheFixer@users.noreply.github.com>
2023-06-11 20:08:35 -0400

    finished movie player

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback } from 'react';
 import { AiOutlineInfoCircle } from 'react-icons/ai'
 
-// import PlayButton from '@/components/PlayButton';
+import PlayButton from './PlayButton';
 import useBillboard from '@/hooks/useBillboard';
 // import useInfoModalStore from '@/hooks/useInfoModalStore';
 
@@ -29,7 +29,7 @@ const Billboard: React.FC = () => {
           {data?.description}
         </p>
         <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
-          {/* <PlayButton movieId={data?.id} /> */}
+          <PlayButton movieId={data?.id} />
           <button
             // onClick={handleOpenModal}
             className="
